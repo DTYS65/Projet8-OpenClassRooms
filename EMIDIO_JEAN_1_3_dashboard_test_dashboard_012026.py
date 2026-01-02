52b4e59a7f4858c9e28cf57a3fdf30c7ac9de100
Jean Emidio <jme1401@hotmail.com>
2026-01-02 18:49:26 +0100

    Version finale

--- EMIDIO_JEAN_1_3_dashboard_test_dashboard_012026.py
+++ EMIDIO_JEAN_1_3_dashboard_test_dashboard_012026.py
@@ -1,15 +1,15 @@
 # TESTS :
 # Ouvrir le terminal sous anaconda, et changer de repertoire :
 # cd C:\Users\jme1401\Desktop\Openclassrooms\7-Implémentez un modèle de scoring\Datas
-# Taper : pytest projet_7_5_TEST_Dashboard.py : lance toutes les fonctions demarrant par test_
+# Taper : pytest main_dashboard_test.py : lance toutes les fonctions demarrant par test_
 
-from projet_7_5_Dashboard import get_prediction
+from main_dashboard import get_prediction
 
 
 def test_get_prediction_accord():
-    assert get_prediction(278288) == (0.238, 'Accordé')
+    assert get_prediction(239207) == (0.407, 'Accordé')
 
 
 def test_get_prediction_refus():
-    assert get_prediction(192535) == (0.545, 'Refusé')
+    assert get_prediction(412720) == (0.542, 'Refusé')
 
